71a1eb9e9e5977a89e396cf94ea21c24ae78eb99
ievbondariev <iev.bondariev@gmail.com>
2021-07-06 17:14:27 +0300

    homework4 task with creating TransactionService

---
+++ src/homework4/exceptions/UserExpectedError.java
@@ -0,0 +1,7 @@
+package homework4.exceptions;
+
+public class UserExpectedError extends RuntimeException {
+    public UserExpectedError(String message) {
+        super(message);
+    }
+}
---
+++ src/homework4/exceptions/WrongFieldException.java
@@ -0,0 +1,7 @@
+package homework4.exceptions;
+
+public class WrongFieldException extends Exception {
+    public WrongFieldException(String message) {
+        super(message);
+    }
+}
